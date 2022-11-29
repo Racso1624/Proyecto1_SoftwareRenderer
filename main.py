6997692d1968c2af6dc8b6a2350b4cbdecf119e8
Racso1624 <fer10_lb@outlook.com>
2022-11-29 01:22:11 -0600

    Se agregan objetos a la escena

--- main.py
+++ main.py
@@ -26,8 +26,14 @@ r.lookAt(V3(0, 0, 10), V3(0, 0, 0), V3(0, 1, 0))
 
 r.light = V3(0, 0, -1)
 
-r.loadModel('./desk.obj', translate=[512, 512, 0], scale=[0.3, 0.3, 0.3], rotate=(0, -pi/6, -pi/2))
+r.loadModel('./desk.obj', translate=[212, 512, 0], scale=[0.3, 0.3, 0.3], rotate=(0, -pi/6, -pi/2))
 
-r.loadModel('./desk_lamp.obj', translate=[512, 812, 0], scale=[400, 400, 400], rotate=(-pi/3, 0, -pi/2))
+r.loadModel('./desk_lamp.obj', translate=[512, 512, 0], scale=[400, 400, 400], rotate=(-pi/3, 0, -pi/2))
+
+r.loadModel('./book.obj', translate=[512, 112, 0], scale=[400, 400, 400], rotate=(-pi/3, -pi/2, -pi/2))
+
+r.loadModel('./glasses.obj', translate=[512, 312, 0], scale=[400, 400, 400], rotate=(0, 0, -pi/2))
+
+r.loadModel('./sharpener.obj', translate=[712, 712, 0], scale=[400, 400, 400], rotate=(0, 0, -pi/2))
 
 r.glFinish("Proyecto_1.bmp")
